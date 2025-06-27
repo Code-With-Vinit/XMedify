62f7f3ac9a331027f420247a98cd938279066dd2
Code-With-Vinit <122153636+Code-With-Vinit@users.noreply.github.com>
2025-06-27 23:59:42 +0530

    Commited Changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,35 @@
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+
+import { createRoot,ReactDOM } from 'react-dom/client'
 import App from './App.jsx'
+// import ReactDOM from "react-dom/client";
+import "./index.css";
+import Search from "./Search/Search";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./Home/Home";
+import { createTheme, ThemeProvider } from "@mui/material";
+import MyBookings from "./MyBookings/MyBookings";
+
+const router= createBrowserRouter ([
+  {
+    path:"/",
+    element:<App/>,
+    children:[
+      {
+        path:"search",
+        element:<Search/>,
+      },
+      {
+        path:"my-bookings",
+        element:<MyBookings/>,
+      },
+      {
+        path:"/",
+        element:<Home/>,
+      },
+    ],
+  },
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
